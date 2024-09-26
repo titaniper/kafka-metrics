fd173da45bb6700f0988ea3976a6776ea8aa451e
titaniper <flutter.kr@gmail.com>
2024-09-26 11:38:53 +0900

    feat: 라이브러리 활용

---
+++ node/src/main.ts
@@ -0,0 +1,43 @@
+import express from 'express';
+import { Kafka } from "kafkajs";
+import { Registry, collectDefaultMetrics } from "prom-client";
+import { monitorKafkaJSProducer, monitorKafkaJSConsumer, monitorKafkaJSAdmin } from "@christiangalsterer/kafkajs-prometheus-exporter";
+
+const app = express();
+const port = 8080;
+
+(async () => {
+    const clientId = 'myClientId'
+    const kafka = new Kafka({
+        clientId: clientId,
+        brokers: ['localhost:9092'],
+    })
+  
+    const producer = kafka.producer()
+    const consumer = kafka.consumer({ groupId: 'myGroupId' })
+  
+    // set up the prometheus client
+    const register = new Registry();
+    collectDefaultMetrics({ register })
+  
+    // @ts-ignore monitor KafkaJS producer 
+    monitorKafkaJSProducer(producer, register, { defaultLabels: {client_id: clientId} })
+    
+    // @ts-ignore monitor KafkaJS consumer
+    monitorKafkaJSConsumer(consumer, register, { defaultLabels: {client_id: clientId} })
+    
+  // connect to Kafka *after* calling monitorKafkaJSProducer() / monitorKafkaJSConsumer / monitorKafkaJSAdmin
+  await producer.connect()
+  await consumer.connect()
+
+   // Expose metrics on /metrics endpoint
+   app.get('/metrics', async (req, res) => {
+    res.set('Content-Type', register.contentType);
+    res.end(await register.metrics());
+});
+
+    // Start server
+    app.listen(port, () => {
+        console.log(`Metrics server listening at http://localhost:${port}/metrics`);
+    });
+})()
